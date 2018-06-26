7d7ccbdf502bb85f2db60eb18376f1256275b7cd
Alexander Marginal <alexander.marginal@gmail.com>
2018-06-26 11:47:43 +0300

    AI
    Компьютер умеет делать ходы, не давать выиграть противнику и находить победный ход для себя.
    
    Теперь нужно:
    - расширить поиск победного хода противника;
    - добавить поиск лучшего хода для AI (пока что он делает ход рандомно);
    - сделать уровни сложности (от количества обдумывания ходов)

--- main.js
+++ main.js
@@ -21,12 +21,12 @@ let p1 = 'p1',
 	emptyCells = '[data-player-mark="0"]',
 	lastStep;
 
-function checkMode (radio) {
+function checkMode(radio) {
 	mode = radio.value;
 }
 
 function init(input) {
-	size=document.getElementById(input).value;
+	size = document.getElementById(input).value;
 	
 	(size > 5) ? winCellNumber = 5 : winCellNumber = size;
 	
@@ -38,20 +38,20 @@ function init(input) {
 }
 
 function initWrapper(wrapperArr, size) {
-	for(let rows=0; rows<size;rows++){
+	for (let rows = 0; rows < size; rows++) {
 		let tableRow = document.createElement('tr');
 		
 		app.appendChild(tableRow);
 		
 		let tableRows = app.getElementsByTagName('tr'),
-			lastTableRow = tableRows[tableRows.length-1];
+			lastTableRow = tableRows[tableRows.length - 1];
 		
 		wrapperArr.push([]);
 		
-		for(let cols=0; cols<size;cols++){
+		for (let cols = 0; cols < size; cols++) {
 			let tableCol = document.createElement('td');
 			
-			tableCol.id="row_"+rows+"_col_"+cols;
+			tableCol.id = "row_" + rows + "_col_" + cols;
 			tableCol.dataset.row = rows;
 			tableCol.dataset.col = cols;
 			tableCol.dataset.playerMark = 0;
@@ -61,7 +61,7 @@ function initWrapper(wrapperArr, size) {
 			lastTableRow.appendChild(tableCol);
 			
 			let colsForLastTableRow = lastTableRow.getElementsByTagName('td'),
-				lastColForLastTableRow = colsForLastTableRow[colsForLastTableRow.length-1];
+				lastColForLastTableRow = colsForLastTableRow[colsForLastTableRow.length - 1];
 			
 			wrapperArr[rows].push(lastColForLastTableRow);
 		}
@@ -69,15 +69,15 @@ function initWrapper(wrapperArr, size) {
 }
 
 function step(cell) {
-	switch (nextStep){
+	switch (nextStep) {
 		case p1:
 			stepAction(cell, 'X');
-			if(mode == ai){
+			if (mode == ai) {
 				
 				nextStep = ai;
 				checkPlayer();
 				
-				if(!app.classList.contains('end')) AI.actionPlanning();
+				if (!app.classList.contains('end')) AI.actionPlanning();
 				
 				break;
 			} else {
@@ -119,29 +119,29 @@ function checkCell(cell, dataMark) {
 	let row = cell.dataset.row,
 		col = cell.dataset.col;
 	
-	count+=1;
+	count += 1;
 	checkTop(+row, +col, dataMark);
 	checkBottom(+row, +col, dataMark);
 	checkWin();
 	
-	count+=1;
+	count += 1;
 	checkLeft(+row, +col, dataMark);
 	checkRight(+row, +col, dataMark);
 	checkWin();
 	
-	count+=1;
+	count += 1;
 	checkTopLeft(+row, +col, dataMark);
 	checkBottomRight(+row, +col, dataMark);
 	checkWin();
 	
-	count+=1;
+	count += 1;
 	checkBottomLeft(+row, +col, dataMark);
 	checkTopRight(+row, +col, dataMark);
 	checkWin();
 	
 	if (document.querySelectorAll(emptyCells).length == 0) {
-		if(!app.classList.contains('end')){
-			changeClickAction ('return false');
+		if (!app.classList.contains('end')) {
+			changeClickAction('return false');
 			
 			app.classList.add('end');
 			end.classList.add('active');
@@ -152,103 +152,111 @@ function checkCell(cell, dataMark) {
 }
 
 function checkTop(cellRow, cellCol, dataMark) {
-	if(cellRow != 0){
+	if (cellRow != 0) {
 		cellRow -= 1;
 		
-		while(cellRow != -1 && getPlayerMark(cellRow,cellCol) == dataMark){
+		while (cellRow != -1 && getPlayerMark(cellRow, cellCol) == dataMark) {
 			cellRow -= 1;
-			count+=1;
+			count += 1;
 		}
 	}
 }
+
 function checkBottom(cellRow, cellCol, dataMark) {
-	if(cellRow != size-1){
+	if (cellRow != size - 1) {
 		cellRow += 1;
 		
-		while(cellRow != size && getPlayerMark(cellRow,cellCol) == dataMark){
+		while (cellRow != size && getPlayerMark(cellRow, cellCol) == dataMark) {
 			cellRow += 1;
-			count+=1;
+			count += 1;
 		}
 	}
 }
+
 function checkLeft(cellRow, cellCol, dataMark) {
-	if(cellCol != 0){
+	if (cellCol != 0) {
 		cellCol -= 1;
 		
-		while(cellCol != -1 && getPlayerMark(cellRow,cellCol) == dataMark){
+		while (cellCol != -1 && getPlayerMark(cellRow, cellCol) == dataMark) {
 			cellCol -= 1;
-			count+=1;
+			count += 1;
 		}
 	}
 }
+
 function checkRight(cellRow, cellCol, dataMark) {
-	if(cellCol != size-1){
+	if (cellCol != size - 1) {
 		cellCol += 1;
 		
-		while(cellCol != size && getPlayerMark(cellRow,cellCol) == dataMark){
+		while (cellCol != size && getPlayerMark(cellRow, cellCol) == dataMark) {
 			cellCol += 1;
-			count+=1;
+			count += 1;
 		}
 	}
 }
+
 function checkTopLeft(cellRow, cellCol, dataMark) {
-	if(cellRow != 0 && cellCol != 0){
+	if (cellRow != 0 && cellCol != 0) {
 		cellRow -= 1;
 		cellCol -= 1;
 		
-		while(cellRow != -1 && cellCol != -1 && getPlayerMark(cellRow,cellCol) == dataMark){
+		while (cellRow != -1 && cellCol != -1 && getPlayerMark(cellRow, cellCol) == dataMark) {
 			cellRow -= 1;
 			cellCol -= 1;
-			count+=1;
+			count += 1;
 		}
 	}
 }
+
 function checkBottomRight(cellRow, cellCol, dataMark) {
-	if(cellRow != size-1 && cellCol != size-1){
+	if (cellRow != size - 1 && cellCol != size - 1) {
 		cellRow += 1;
 		cellCol += 1;
 		
-		while(cellRow != size && cellCol != size && getPlayerMark(cellRow,cellCol) == dataMark){
+		while (cellRow != size && cellCol != size && getPlayerMark(cellRow, cellCol) == dataMark) {
 			cellRow += 1;
 			cellCol += 1;
-			count+=1;
+			count += 1;
 		}
 	}
 }
+
 function checkBottomLeft(cellRow, cellCol, dataMark) {
-	if(cellRow != size-1 && cellCol != 0){
+	if (cellRow != size - 1 && cellCol != 0) {
 		cellRow += 1;
 		cellCol -= 1;
 		
-		while(cellRow != size && cellCol != -1 && getPlayerMark(cellRow,cellCol) == dataMark){
+		while (cellRow != size && cellCol != -1 && getPlayerMark(cellRow, cellCol) == dataMark) {
 			cellRow += 1;
 			cellCol -= 1;
-			count+=1;
+			count += 1;
 		}
 	}
 }
+
 function checkTopRight(cellRow, cellCol, dataMark) {
-	if(cellRow != 0 && cellCol != size-1){
+	if (cellRow != 0 && cellCol != size - 1) {
 		cellRow -= 1;
 		cellCol += 1;
 		
-		while(cellRow != -1 && cellCol != size && getPlayerMark(cellRow,cellCol) == dataMark){
+		while (cellRow != -1 && cellCol != size && getPlayerMark(cellRow, cellCol) == dataMark) {
 			cellRow -= 1;
 			cellCol += 1;
-			count+=1;
+			count += 1;
 		}
 	}
 }
+
 /* END CHECK LINE */
 
 function checkWin() {
-	if(count >= winCellNumber){
-		changeClickAction ('return false');
+	if (count >= winCellNumber) {
+		changeClickAction('return false');
 		
 		app.classList.add('end');
 		end.classList.add('active');
 		
-		if(nextStep == p1){
+		if (nextStep == p1) {
 			endText.innerHTML = 'Победил игрок 1';
 		} else if (nextStep == ai) {
 			endText.innerHTML = 'Победил ИИ';
@@ -257,19 +265,19 @@ function checkWin() {
 		}
 	}
 	
-	count=0;
+	count = 0;
 }
 
 function restart() {
 	app.classList.remove('end');
-	app.innerHTML='';
+	app.innerHTML = '';
 	end.classList.remove('active');
 	endText.innerHTML = '';
 	ask.classList.remove('start');
 }
 
 function checkPlayer() {
-	switch (nextStep){
+	switch (nextStep) {
 		case p1:
 			player1.classList.add('active');
 			player2.classList.remove('active');
@@ -281,17 +289,18 @@ function checkPlayer() {
 	}
 }
 
-function changeClickAction (action) {
-	for(let rows=0; rows<wrapperArr.length;rows++){
-		for(let cols=0; cols<wrapperArr[rows].length;cols++){
+function changeClickAction(action) {
+	for (let rows = 0; rows < wrapperArr.length; rows++) {
+		for (let cols = 0; cols < wrapperArr[rows].length; cols++) {
 			wrapperArr[rows][cols].setAttribute('onclick', action);
 		}
 	}
 }
 
-function getPlayerMark(row,col) {
-	return document.getElementById('row_'+row+'_col_'+col).dataset.playerMark;
+function getPlayerMark(row, col) {
+	return document.getElementById('row_' + row + '_col_' + col).dataset.playerMark;
 }
+
 function rand(min, max) {
 	return min + Math.floor(Math.random() * (max + 1 - min));
 }
@@ -299,165 +308,255 @@ function rand(min, max) {
 
 /* AI */
 const AI = {
-	count:0,
+	count: 0,
 	
-	actionPlanning:function () {
+	actionPlanning: function () {
 		let stepVars = document.querySelectorAll(emptyCells);
 		
 		// First step
-		if (stepVars.length == size*size) {
-			step(stepVars[rand(0, stepVars.length-1)]);
+		if (stepVars.length == size * size || stepVars.length == (size * size)-1) {
+			step(stepVars[rand(0, stepVars.length - 1)]);
 		} else {
-			this.findWinStep(p2Mark, 'O'); //step for WIN
-			this.findWinStep(p1Mark, 'X'); //step for not LOOSE
+			/* START STEP FOR WIN */
+			this.findWinStep(p2Mark, 'O');
+			if (nextStep != ai) return false;
+			/* END STEP FOR WIN */
+			
+			/* START STEP FOR NOT LOOSE */
+			this.findWinStep(p1Mark, 'X');
+			if (nextStep != ai) return false;
+			/* END STEP FOR NOT LOOSE */
+			
+			//TODO доделать поиск лучшего хода, а пока что рандом
+			/* START BEST MOVE */
+			/*this.findBestMove(stepVars);
+			if (nextStep != ai) return false;*/
+			/* END BEST MOVE */
+			step(stepVars[rand(0, stepVars.length - 1)]);
 		}
 	},
 	
-	/* START FIND WIN/LOOSE STEP */
-	getStepCell: function (row,col) {
-		return document.getElementById("row_"+row+"_col_"+col);
+	
+	getStepCell: function (row, col) {
+		return document.getElementById("row_" + row + "_col_" + col);
 	},
-	findEmptyCellForStep: function(stepRow,stepCol,dataMark){
-		if (this.getStepCell(stepRow,stepCol) && getPlayerMark(stepRow,stepCol)==0){
-			console.log(this.getStepCell(stepRow,stepCol));
+	
+	findEmptyCellForStep: function (stepRow, stepCol, dataMark) {
+		if (this.getStepCell(stepRow, stepCol) && getPlayerMark(stepRow, stepCol) == 0) {
 			
-			this.count+=1;
-			this.findTop(stepRow,stepCol,dataMark);
-			this.findBottom(stepRow,stepCol,dataMark);
-			this.thisStepWin(stepRow,stepCol);
+			this.count += 1;
+			this.findTop(stepRow, stepCol, dataMark);
+			this.findBottom(stepRow, stepCol, dataMark);
+			this.thisStepWin(stepRow, stepCol);
 			
-			this.count+=1;
-			this.findLeft(stepRow,stepCol,dataMark);
-			this.findRight(stepRow,stepCol,dataMark);
-			this.thisStepWin(stepRow,stepCol);
+			this.count += 1;
+			this.findLeft(stepRow, stepCol, dataMark);
+			this.findRight(stepRow, stepCol, dataMark);
+			this.thisStepWin(stepRow, stepCol);
 			
-			this.count+=1;
-			this.findTopLeft(stepRow,stepCol,dataMark);
-			this.findBottomRight(stepRow,stepCol,dataMark);
-			this.thisStepWin(stepRow,stepCol);
+			this.count += 1;
+			this.findTopLeft(stepRow, stepCol, dataMark);
+			this.findBottomRight(stepRow, stepCol, dataMark);
+			this.thisStepWin(stepRow, stepCol);
 			
-			this.count+=1;
-			this.findBottomLeft(stepRow,stepCol,dataMark);
-			this.findTopRight(stepRow,stepCol,dataMark);
-			this.thisStepWin(stepRow,stepCol);
+			this.count += 1;
+			this.findBottomLeft(stepRow, stepCol, dataMark);
+			this.findTopRight(stepRow, stepCol, dataMark);
+			this.thisStepWin(stepRow, stepCol);
 		}
 	},
 	
-	findWinStep: function (dataMark,pMark) {
+	findWinStep: function (dataMark, pMark) {
 		let iOccupiedCell = document.querySelectorAll(dataMark);
 		
-		for(let i=0;i<iOccupiedCell.length;i++){
+		for (let i = 0; i < iOccupiedCell.length; i++) {
 			let thisCell = iOccupiedCell[i],
 				row = +thisCell.dataset.row,
 				col = +thisCell.dataset.col;
-
-			this.findEmptyCellForStep(row-1,col-1,pMark);
-			this.findEmptyCellForStep(row-1,col,pMark);
-			this.findEmptyCellForStep(row-1,col+1,pMark);
-			this.findEmptyCellForStep(row+1,col-1,pMark);
-			this.findEmptyCellForStep(row+1,col,pMark);
-			this.findEmptyCellForStep(row+1,col+1,pMark);
-			this.findEmptyCellForStep(row,col-1,pMark);
-			this.findEmptyCellForStep(row,col+1,pMark);
+			
+			this.findEmptyCellForStep(row - 1, col - 1, pMark);
+			this.findEmptyCellForStep(row - 1, col, pMark);
+			this.findEmptyCellForStep(row - 1, col + 1, pMark);
+			this.findEmptyCellForStep(row + 1, col - 1, pMark);
+			this.findEmptyCellForStep(row + 1, col, pMark);
+			this.findEmptyCellForStep(row + 1, col + 1, pMark);
+			this.findEmptyCellForStep(row, col - 1, pMark);
+			this.findEmptyCellForStep(row, col + 1, pMark);
 			
 		}
 	},
 	
-	findTop: function(cellRow, cellCol, dataMark) {
-	if(cellRow != 0){
-		cellRow -= 1;
-		
-		while(cellRow != -1 && getPlayerMark(cellRow,cellCol) == dataMark){
-			cellRow -= 1;
-			this.count+=1;
+	thisStepWin: function (row, col) {
+		if (this.count >= winCellNumber) {
+			step(this.getStepCell(row, col));
 		}
-	}
-},
-	findBottom: function(cellRow, cellCol, dataMark) {
-	if(cellRow != size-1){
-		cellRow += 1;
 		
-		while(cellRow != size && getPlayerMark(cellRow,cellCol) == dataMark){
-			cellRow += 1;
-			AI.count+=1;
-		}
-	}
-},
-	findLeft: function(cellRow, cellCol, dataMark) {
-	if(cellCol != 0){
-		cellCol -= 1;
-		
-		while(cellCol != -1 && getPlayerMark(cellRow,cellCol) == dataMark){
-			cellCol -= 1;
-			this.count+=1;
-		}
-	}
-},
-	findRight: function(cellRow, cellCol, dataMark) {
-	if(cellCol != size-1){
-		cellCol += 1;
-		
-		while(cellCol != size && getPlayerMark(cellRow,cellCol) == dataMark){
-			cellCol += 1;
-			this.count+=1;
-		}
-	}
-},
-	findTopLeft: function(cellRow, cellCol, dataMark) {
-	if(cellRow != 0 && cellCol != 0){
-		cellRow -= 1;
-		cellCol -= 1;
-		
-		while(cellRow != -1 && cellCol != -1 && getPlayerMark(cellRow,cellCol) == dataMark){
-			cellRow -= 1;
-			cellCol -= 1;
-			this.count+=1;
-		}
-	}
-},
-	findBottomRight: function(cellRow, cellCol, dataMark) {
-	if(cellRow != size-1 && cellCol != size-1){
-		cellRow += 1;
-		cellCol += 1;
-		
-		while(cellRow != size && cellCol != size && getPlayerMark(cellRow,cellCol) == dataMark){
-			cellRow += 1;
-			cellCol += 1;
-			this.count+=1;
-		}
-	}
-},
-	findBottomLeft: function(cellRow, cellCol, dataMark) {
-	if(cellRow != size-1 && cellCol != 0){
-		cellRow += 1;
-		cellCol -= 1;
-		
-		while(cellRow != size && cellCol != -1 && getPlayerMark(cellRow,cellCol) == dataMark){
-			cellRow += 1;
-			cellCol -= 1;
-			this.count+=1;
-		}
-	}
-},
-	findTopRight: function(cellRow, cellCol, dataMark) {
-	if(cellRow != 0 && cellCol != size-1){
-		cellRow -= 1;
-		cellCol += 1;
-		
-		while(cellRow != -1 && cellCol != size && getPlayerMark(cellRow,cellCol) == dataMark){
-			cellRow -= 1;
-			cellCol += 1;
-			this.count+=1;
-		}
-	}
-},
+		this.count = 0;
+	},
 	
-	thisStepWin:function (row,col) {
-		if(this.count >= winCellNumber){
-			step(this.getStepCell(row,col));
+	findTop: function (cellRow, cellCol, dataMark) {
+		if (cellRow != 0) {
+			cellRow -= 1;
+			
+			while (cellRow != -1 && getPlayerMark(cellRow, cellCol) == dataMark) {
+				cellRow -= 1;
+				this.count += 1;
+			}
+		}
+	},
+	findBottom: function (cellRow, cellCol, dataMark) {
+		if (cellRow != size - 1) {
+			cellRow += 1;
+			
+			while (cellRow != size && getPlayerMark(cellRow, cellCol) == dataMark) {
+				cellRow += 1;
+				AI.count += 1;
+			}
+		}
+	},
+	findLeft: function (cellRow, cellCol, dataMark) {
+		if (cellCol != 0) {
+			cellCol -= 1;
+			
+			while (cellCol != -1 && getPlayerMark(cellRow, cellCol) == dataMark) {
+				cellCol -= 1;
+				this.count += 1;
+			}
+		}
+	},
+	findRight: function (cellRow, cellCol, dataMark) {
+		if (cellCol != size - 1) {
+			cellCol += 1;
+			
+			while (cellCol != size && getPlayerMark(cellRow, cellCol) == dataMark) {
+				cellCol += 1;
+				this.count += 1;
+			}
+		}
+	},
+	findTopLeft: function (cellRow, cellCol, dataMark) {
+		if (cellRow != 0 && cellCol != 0) {
+			cellRow -= 1;
+			cellCol -= 1;
+			
+			while (cellRow != -1 && cellCol != -1 && getPlayerMark(cellRow, cellCol) == dataMark) {
+				cellRow -= 1;
+				cellCol -= 1;
+				this.count += 1;
+			}
+		}
+	},
+	findBottomRight: function (cellRow, cellCol, dataMark) {
+		if (cellRow != size - 1 && cellCol != size - 1) {
+			cellRow += 1;
+			cellCol += 1;
+			
+			while (cellRow != size && cellCol != size && getPlayerMark(cellRow, cellCol) == dataMark) {
+				cellRow += 1;
+				cellCol += 1;
+				this.count += 1;
+			}
+		}
+	},
+	findBottomLeft: function (cellRow, cellCol, dataMark) {
+		if (cellRow != size - 1 && cellCol != 0) {
+			cellRow += 1;
+			cellCol -= 1;
+			
+			while (cellRow != size && cellCol != -1 && getPlayerMark(cellRow, cellCol) == dataMark) {
+				cellRow += 1;
+				cellCol -= 1;
+				this.count += 1;
+			}
+		}
+	},
+	findTopRight: function (cellRow, cellCol, dataMark) {
+		if (cellRow != 0 && cellCol != size - 1) {
+			cellRow -= 1;
+			cellCol += 1;
+			
+			while (cellRow != -1 && cellCol != size && getPlayerMark(cellRow, cellCol) == dataMark) {
+				cellRow -= 1;
+				cellCol += 1;
+				this.count += 1;
+			}
+		}
+	},
+	
+	findBestMove: function (emptyCellsArr) {
+		console.log(emptyCellsArr);
+		let stepsArr=[];
+		
+		for (let stepVar=0; stepVar < emptyCellsArr.length; stepVar++) {
+			/* START STEP FOR WIN */
+			find(p2Mark, 'O');
+			if (nextStep != ai) return false;
+			/* END STEP FOR WIN */
+			
+			/* START STEP FOR NOT LOOSE */
+			/*find(p1Mark, 'X');
+			if (nextStep != ai) return false;*/
+			/* END STEP FOR NOT LOOSE */
 		}
 		
-		this.count=0;
-	}
-	/* END FIND WIN/LOOSE STEP */
+		/* THIS STEP FUNCTIONS */
+		function find(dataMark, pMark){
+			let iOccupiedCell = document.querySelectorAll(dataMark);
+			
+			for (let i = 0; i < iOccupiedCell.length; i++) {
+				let thisCell = iOccupiedCell[i],
+					row = +thisCell.dataset.row,
+					col = +thisCell.dataset.col;
+				
+				findEmptyCell(row - 1, col - 1, pMark);
+				findEmptyCell(row - 1, col, pMark);
+				findEmptyCell(row - 1, col + 1, pMark);
+				findEmptyCell(row + 1, col - 1, pMark);
+				findEmptyCell(row + 1, col, pMark);
+				findEmptyCell(row + 1, col + 1, pMark);
+				findEmptyCell(row, col - 1, pMark);
+				findEmptyCell(row, col + 1, pMark);
+				
+			}
+		}
+		function findEmptyCell(stepRow, stepCol, dataMark) {
+			if (AI.getStepCell(stepRow, stepCol) && getPlayerMark(stepRow, stepCol) == 0) {
+				
+				this.count += 1;
+				AI.findTop(stepRow, stepCol, dataMark);
+				AI.findBottom(stepRow, stepCol, dataMark);
+				console.log(count);
+				thisMoveIsBest(stepRow, stepCol);
+				
+				this.count += 1;
+				AI.findLeft(stepRow, stepCol, dataMark);
+				AI.findRight(stepRow, stepCol, dataMark);
+				console.log(count);
+				thisMoveIsBest(stepRow, stepCol);
+				
+				this.count += 1;
+				AI.findTopLeft(stepRow, stepCol, dataMark);
+				AI.findBottomRight(stepRow, stepCol, dataMark);
+				console.log(count);
+				thisMoveIsBest(stepRow, stepCol);
+				
+				this.count += 1;
+				AI.findBottomLeft(stepRow, stepCol, dataMark);
+				AI.findTopRight(stepRow, stepCol, dataMark);
+				console.log(count);
+				thisMoveIsBest(stepRow, stepCol);
+			}
+		}
+		function thisMoveIsBest(row, col) {
+			stepsArr.push({} = new MoveVar(row,col,count));
+			
+			this.count = 0;
+			
+			console.log(stepsArr);
+		}
+		function MoveVar(row,col,score) {
+			this.cell = AI.getStepCell(row,col);
+			this.score=score;
+		}
+	},
 };
